3643d11d70a0af8b0fce465f14492955a7d14469
GaboSanti <20243rd011@utez.edu.mx>
2025-09-17 16:04:50 -0600

    Print the founded repositories

--- gitHub_Scanner.py
+++ gitHub_Scanner.py
@@ -11,3 +11,11 @@ git_dir = r"C:\Users\mikel\OneDrive\Escritorio\Gabo\UTEZ\4to\progra de redes\pra
 repos = [name for name in os.listdir(git_dir)
          if os.path.isdir(os.path.join(git_dir, name)) and
          os.path.exists(os.path.join(git_dir, name, ".git"))]
+
+if repos:
+    print("found the following GitHub repositories in ",git_dir)
+    for idx, repo in enumerate(repos, 1):
+        print(f"{idx}, {repo}")
+else: 
+    print("No GitHub repositories found in ", git_dir)
+    print("please provide a GitHub repository")
